f0ea89de2fa12de23c28d09ff8f29949d0b7a8b1
dantistas <rytisvenslovas@gmail.com>
2020-05-14 13:16:00 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,13 @@
 const dummy = (blog) => {
     return 1
   }
-  
+const totalLikes = (blog) => {
+    const reducer = (sum, likes)=>{
+        return sum + likes.likes
+    }
+    return blog.reduce(reducer,0) 
+}  
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
